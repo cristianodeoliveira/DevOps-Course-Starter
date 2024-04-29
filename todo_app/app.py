1f890933d5a93aca815565592f21db1031956ff4
cristianodeoliveira <66634829+cristianodeoliveira@users.noreply.github.com>
2024-04-29 17:03:20 +0100

    Able to add new items to the list.

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,14 +1,20 @@
-from flask import Flask, render_template
-from todo_app.data.session_items import get_items
+from crypt import methods
+from flask import Flask, redirect, render_template, request
+from todo_app.data.session_items import add_item, get_items
 
 from todo_app.flask_config import Config
 
 app = Flask(__name__)
 app.config.from_object(Config())
 
-
 @app.route('/')
 def index():
     #return 'Hello World!!!!'
     items = get_items()
     return render_template ('index.html', items = items)
+
+@app.route('/add-todo', methods=["POST"])
+def add_todo():
+    new_todo_title = request.form.get('title')
+    add_item(new_todo_title)
+    return redirect('/')
