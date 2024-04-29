372acfdfc84fc6cd20d49f69f827cceabb13d28a
cristianodeoliveira <66634829+cristianodeoliveira@users.noreply.github.com>
2024-04-29 16:34:29 +0100

    Show list of items

--- todo_app/data/session_items.py
+++ todo_app/data/session_items.py
@@ -2,7 +2,9 @@ from flask import session
 
 _DEFAULT_ITEMS = [
     { 'id': 1, 'status': 'Not Started', 'title': 'List saved todo items' },
-    { 'id': 2, 'status': 'Not Started', 'title': 'Allow new items to be added' }
+    { 'id': 2, 'status': 'Not Started', 'title': 'Allow new items to be added' },
+    { 'id': 3,'status': 'Started', 'title': 'M1 project' },
+    { 'id': 4,'status': 'Not Started', 'title': 'Speak to Jack' }
 ]
 
 
