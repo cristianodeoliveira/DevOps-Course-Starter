59c1083c0ff88447d6c1389a845c2da99a38cf0c
cristianodeoliveira <66634829+cristianodeoliveira@users.noreply.github.com>
2024-05-02 13:40:35 +0100

    move key and token to .env file

--- create_item_demo.py
+++ create_item_demo.py
@@ -1,21 +1,19 @@
 import requests
+import dotenv
+import os
+
+dotenv.load_dotenv()
+
 
 reqUrl = "https://api.trello.com/1/cards"
 
 query_params = {
-    "key":"ff02dc85cb92b92ec769718e2a6befa6",
-    "token":"ff02dc85cb92b92ec769718e2a6befa6",
-    "idlist":"6630eca0d794a3515318f326",
-    "name":"Python Created Todo"
+    "key": os.getenv("TRELLO_API_KEY"),
+    "token": os.getenv("TRELLO_API_TOKEN"),
+    "idList": "6630eca10b5e88d30c557a24",
+    "name" : "Python Created Todo 2"
 }
 
-headersList = {
- "Accept": "*/*",
- "User-Agent": "Thunder Client (https://www.thunderclient.com)" 
-}
-
-payload = ""
-
-response = requests.post(reqUrl, data=payload,  headers=headersList, params = query_params)
+response = requests.post(reqUrl,  params = query_params)
 
 print(response.text)
