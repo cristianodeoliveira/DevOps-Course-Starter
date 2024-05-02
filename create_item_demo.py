fbb711d4a78755615669c590306ba59c40373c21
cristianodeoliveira <66634829+cristianodeoliveira@users.noreply.github.com>
2024-05-02 14:08:04 +0100

    Able to create from Website into Trello via API

--- create_item_demo.py
+++ create_item_demo.py
@@ -10,8 +10,8 @@ reqUrl = "https://api.trello.com/1/cards"
 query_params = {
     "key": os.getenv("TRELLO_API_KEY"),
     "token": os.getenv("TRELLO_API_TOKEN"),
-    "idList": "6630eca10b5e88d30c557a24",
-    "name" : "Python Created Todo 2"
+    "idList": os.getenv("TRELLO_TODO_LIST_ID"),
+    "name" : "Python Created Todo 3"
 }
 
 response = requests.post(reqUrl,  params = query_params)
