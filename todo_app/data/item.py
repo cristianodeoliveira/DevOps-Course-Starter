80bd113761e5f099acd48eb5afafb14029f1a015
cristianodeoliveira <66634829+cristianodeoliveira@users.noreply.github.com>
2024-05-03 19:00:06 +0100

    New classes creates, not sure I fully understand now right now

---
+++ todo_app/data/item.py
@@ -0,0 +1,9 @@
+class Item:
+    def __init__(self, id, name, status='To Do'):
+        self.id = id
+        self.name = name
+        self.status = status
+
+    @classmethod
+    def from_trello_card (cls, card, list):
+        return Item(card['id'],card['name'],list['name'])
---
+++ todo_app/data/view_model.py
@@ -0,0 +1,9 @@
+from todo_app.data.item import Item
+
+class ViewModel:
+    def __init__(self, items: list[Item]):
+        self._items = items
+ 
+    #@property
+    def items(self):
+        return self._items
