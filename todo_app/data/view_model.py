a9fb08c3e3cdb8a8afc00e42873304d34409c69b
cristianodeoliveira <66634829+cristianodeoliveira@users.noreply.github.com>
2024-05-06 14:23:22 +0100

    It contains new test case

--- todo_app/data/view_model.py
+++ todo_app/data/view_model.py
@@ -5,13 +5,25 @@ class ViewModel:
         self._items = items
  
     @property
-    def todo_items(self):
-        return []
+    def todo_items(self) -> list[Item]:
+        output = []
+        for item in self._items:
+            if item.status == "To Do":
+                output.append(item)
+        return output:
     
     @property
-    def doing_items(self):
-        return []
-    
+    def doing_items(self) -> list[Item]:
+        output = []
+        for item in self._items:
+            if item.status == "Doing":
+                output.append(item)
+        return output:
+
     @property
-    def done_items(self):
-        return []
+    def done_items(self) -> list[Item]:
+        output = []
+        for item in self._items:
+            if item.status == "Done":
+                output.append(item)
+        return output
