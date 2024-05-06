005ad312e5dcc4ff9e408a3a2c425bf8db00436a
cristianodeoliveira <66634829+cristianodeoliveira@users.noreply.github.com>
2024-05-06 14:36:21 +0100

    new changes

--- todo_app/app.py
+++ todo_app/app.py
@@ -5,22 +5,25 @@ from todo_app.data.view_model import ViewModel
 
 from todo_app.flask_config import Config
 
-app = Flask(__name__)
-app.config.from_object(Config())
+def create_app()
+    app = Flask(__name__)
+    app.config.from_object(Config())
 
-@app.route('/')
-def index():
-    items = get_items()
-    view_model = ViewModel(items)
-    return render_template ('index.html', view_model = view_model)
+    @app.route('/')
+    def index():
+        items = get_items()
+        view_model = ViewModel(items)
+        return render_template ('index.html', view_model = view_model)
 
-@app.route('/add-todo', methods=["POST"])
-def add_todo():
-    new_todo_title = request.form.get('title')
-    add_item(new_todo_title)
-    return redirect('/')
+    @app.route('/add-todo', methods=["POST"])
+    def add_todo():
+        new_todo_title = request.form.get('title')
+        add_item(new_todo_title)
+        return redirect('/')
 
-@app.route('/complete-item/<todo_id>', methods=["POST"])
-def complete_item (todo_id):
-    move_item_to_done(todo_id)
-    return redirect('/')
+    @app.route('/complete-item/<todo_id>', methods=["POST"])
+    def complete_item (todo_id):
+        move_item_to_done(todo_id)
+        return redirect('/')
+
+    return app
