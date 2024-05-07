a73cd2d7d1c56d11b1d1b8dd50dcacab4f4db03f
cristianodeoliveira <66634829+cristianodeoliveira@users.noreply.github.com>
2024-05-07 18:05:24 +0100

    I am not able to fix the issues with the test :-(

--- todo_app/data/view_model.py
+++ todo_app/data/view_model.py
@@ -26,6 +26,7 @@ class ViewModel:
     @property
     def done_items(self) -> list[Item]:
         output = []
+        
         for item in self._items:
             if item[("status")] == "Done":
                 output.append(item)
