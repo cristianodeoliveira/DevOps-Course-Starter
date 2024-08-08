235718c8d105b6bf8aa6bc15275a6fe57ae24dbf
cristianodeoliveira <66634829+cristianodeoliveira@users.noreply.github.com>
2024-08-08 18:30:22 +0100

    Retriving entries from mongodb directly :-)

--- todo_app/data/mongo_entries.py
+++ todo_app/data/mongo_entries.py
@@ -1,6 +1,8 @@
 import pymongo
 import os
 
+from todo_app.data.item import Item
+
 client = pymongo.MongoClient (os.getenv("MONGODB_CONNECTION_STRING"))
 
 db = client[os.getenv("MONGODB_DB_NAME")]
@@ -14,8 +16,16 @@ def add_item(new_todo_title: str):
     }
     collection.insert_one(new_item)
 
-def get_item():
-    pass
+def get_items():
+    mongodb_documents = list(collection.find())
+    
+    items = []
+    for document in mongodb_documents:
+        item = Item.from_mongo_document(document)
+        items.append(item)
+    
+    return items
+        
 
 def move_item_to_done():
     pass
