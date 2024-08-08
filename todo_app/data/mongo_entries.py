4bcbf479fedbbd601f14a15b16db4bfbdd637746
cristianodeoliveira <66634829+cristianodeoliveira@users.noreply.github.com>
2024-08-08 20:25:26 +0100

    completed the test, I was getting an error and I had to install poetry add mongomock setuptools

--- todo_app/data/mongo_entries.py
+++ todo_app/data/mongo_entries.py
@@ -4,13 +4,18 @@ import os
 
 from todo_app.data.item import Item
 
-client = pymongo.MongoClient (os.getenv("MONGODB_CONNECTION_STRING"))
+def get_collection():
+    client = pymongo.MongoClient (os.getenv("MONGODB_CONNECTION_STRING"))
 
-db = client[os.getenv("MONGODB_DB_NAME")]
-collection = db[os.getenv("MONGODB_COLLECTION_NAME")]
+    db = client[os.getenv("MONGODB_DB_NAME")]
+    collection = db[os.getenv("MONGODB_COLLECTION_NAME")]
+    
+    return collection
 
 
 def add_item(new_todo_title: str):
+    collection = get_collection()
+    
     new_item = {
         "name":new_todo_title,
         "status":"To Do"
@@ -18,6 +23,7 @@ def add_item(new_todo_title: str):
     collection.insert_one(new_item)
 
 def get_items():
+    collection = get_collection()
     mongodb_documents = list(collection.find())
     
     items = []
@@ -29,5 +35,6 @@ def get_items():
         
 
 def move_item_to_done(todo_id: str):
+    collection = get_collection()
+    
     collection.update_one({"_id":ObjectId(todo_id)},{"$set":{"status":"Done"}})
-    pass
