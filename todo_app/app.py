ce5a29537ac7475984b85a3c98ab0a2411a1d89a
cristianodeoliveira <66634829+cristianodeoliveira@users.noreply.github.com>
2024-10-10 18:24:05 +0100

    Commit including Loggly logging

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,16 +1,22 @@
 import os
 from flask import Flask, redirect, render_template, request
 from todo_app.data.mongo_entries import add_item, get_items, move_item_to_done
-
 from todo_app.data.view_model import ViewModel
-
 from todo_app.flask_config import Config
+from loggly.handlers import HTTPSHandler
+from logging import Formatter
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config())
-    #app.logger.setLevel(app.config['LOG_LEVEL'])
     app.logger.setLevel(os.environ.get('LOG_LEVEL'))
+    
+    if app.config['LOGGLY_TOKEN'] is not None:
+        handler = HTTPSHandler(f'https://logs-01.loggly.com/inputs/{app.config["LOGGLY_TOKEN"]}/tag/todo-app')
+        handler.setFormatter(
+            Formatter("[%(asctime)s] %(levelname)s in %(module)s: %(message)s")
+        )
+        app.logger.addHandler(handler)
     
     @app.route('/')
     def index():
@@ -23,7 +29,7 @@ def create_app():
     def add_todo():
         new_todo_title = request.form.get('title')
         add_item(new_todo_title)
-        app.logger.info(f"Added a new todo item {new_todo_title}")
+        app.logger.info(f"Added a new todo item - {new_todo_title}")
         return redirect('/')
 
     @app.route('/complete-item/<todo_id>', methods=["POST"])
--- todo_app/flask_config.py
+++ todo_app/flask_config.py
@@ -4,5 +4,6 @@ class Config:
     def __init__(self):
         """Base configuration variables."""
         self.SECRET_KEY = os.environ.get('SECRET_KEY')
+        self.LOGGLY_TOKEN = os.environ.get('LOGGLY_TOKEN')
         if not self.SECRET_KEY:
             raise ValueError("No SECRET_KEY set for Flask application. Did you follow the setup instructions?")
